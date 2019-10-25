852900e02f90a118c5aa846a21938e7165b99e32
naichuan-zhang <zhangnaichuan168@gmail.com>
2019-10-25 10:19:56 +0100

    fixed

--- src/main/java/com/cyan/hotel/HotelApplication.java
+++ src/main/java/com/cyan/hotel/HotelApplication.java
@@ -2,9 +2,18 @@ package com.cyan.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HotelApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Home";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HotelApplication.class, args);
