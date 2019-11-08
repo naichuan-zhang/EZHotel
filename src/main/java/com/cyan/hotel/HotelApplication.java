595ba9a431137f25f256d264157a8c2c9e0d87e4
John Long <John Long>
2019-11-08 01:25:48 +0000

    Adding room type entity to test database

--- src/main/java/com/cyan/hotel/HotelApplication.java
+++ src/main/java/com/cyan/hotel/HotelApplication.java
@@ -1,13 +1,27 @@
 package com.cyan.hotel;
 
+import com.cyan.hotel.model.RoomType;
+import com.cyan.hotel.repository.RoomTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 public class HotelApplication extends SpringBootServletInitializer {
+    @Autowired
+    RoomTypeRepository roomTypeRepository;
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -16,6 +30,16 @@ public class HotelApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HotelApplication.class, args);
+
+        List<RoomType> roomTypes = new ArrayList<>();
+        for(RoomType roomType : roomTypes){
+            System.out.println(roomType.getRoomType());
+        }
+
+    }
+
+    public List<RoomType> getAllRoomTypes(){
+        return roomTypeRepository.findAll();
     }
 
 }
---
+++ src/main/java/com/cyan/hotel/repository/RoomTypeRepository.java
@@ -0,0 +1,8 @@
+package com.cyan.hotel.repository;
+
+import com.cyan.hotel.model.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTypeRepository extends JpaRepository<RoomType, Long> {
+
+}
