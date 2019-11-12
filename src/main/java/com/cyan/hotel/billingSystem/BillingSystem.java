0775c9cf6cb6ab9d29a9116aa197769a9332b448
John Long <John Long>
2019-11-12 22:55:50 +0000

    Fixed registration error

---
+++ src/main/java/com/cyan/hotel/billingSystem/BillingSystem.java
@@ -0,0 +1,12 @@
+package com.cyan.hotel.billingSystem;
+
+/**
+ * @author: John Long
+ * @create: 12-Nov-2019
+ **/
+
+public class BillingSystem {
+    public void updateBill() {
+
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ROOM_TYPE VALUES
+('1', '3','Junior Suite', '2', '200','junior suite'),
+('2', '3','Executive Suite', '2', '350','executive suite');
